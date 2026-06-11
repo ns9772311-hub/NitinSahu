f4d08ff4939310f38dc0fffde38dda85c06f94b4
Nitin Sahu <ns9772311@gmail.com>
2026-06-11 23:14:46 +0530

    First portfolio commit

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
                 <h1 className="hero-greeting">Hi, I'm <span className="highlight">NITIN SAHU</span></h1>
                 <p className="hero-description">Web Developer | Cyber security & ethical hacking</p>
                 <a href="#"
-                download={"Nitin_sahu_CV.pdf"}
+                download={"nitinresume.pdf"}
                 className="btn hero-btn"
                 style={{ textDecoration: 'none', display: 'inline-block' }}>
                     Download CV
